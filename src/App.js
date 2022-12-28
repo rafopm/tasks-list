567baa502dd9d6a7817006943cb0fc838011f26d
rafopm <rafopm@gmail.com>
2022-12-27 22:54:19 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ListaDeTareas from './components/ListaDeTareas';
+
+
+function App() {
+  return (
+    <div className="app-tareas">
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
